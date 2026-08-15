db1322021107f4a8e8661b37235a5c908501c4f4
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-08-15 22:34:25 +0530

    Add Java Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/838. Design Linked List/Solution.java
+++ LeetCode/Patterns/Linked List/838. Design Linked List/Solution.java
@@ -11,62 +11,57 @@ class MyLinkedList {
     int size;
     public MyLinkedList() {
         head = null;
-        size = 0;  
+        size = 0;
     }
-    
     public int get(int index) {
-        if(index < 0 || index > size) return -1;
+        if (index < 0 || index >= size)
+            return -1;
         Node curr = head;
-        for(int i = 0 ; i < size ; i++){
+        for (int i = 0; i < index; i++) {
             curr = curr.next;
         }
         return curr.val;
     }
-    
     public void addAtHead(int val) {
         Node newNode = new Node(val);
         newNode.next = head;
         head = newNode;
-
         size++;
     }
-    
     public void addAtTail(int val) {
         Node newNode = new Node(val);
-        if(head == null){
+        if (head == null) {
             head = newNode;
             size++;
             return;
-        } 
-
+        }
         Node curr = head;
-        while(curr.next != null){
+        while (curr.next != null) {
             curr = curr.next;
         }
         curr.next = newNode;
         size++;
     }
-    
     public void addAtIndex(int index, int val) {
-        if(index < 0 || index > size) return;
-        if(index == 0){
+        if (index < 0 || index > size)
+            return;
+        if (index == 0) {
             addAtHead(val);
             return;
         }
-        if(index == size){
+        if (index == size) {
             addAtTail(val);
             return;
         }
         Node newNode = new Node(val);
         Node curr = head;
-        for(int i = 0 ; i < index - 1 ; i++){
+        for (int i = 0; i < index - 1; i++) {
             curr = curr.next;
         }
         newNode.next = curr.next;
         curr.next = newNode;
         size++;
     }
-    
     public void deleteAtIndex(int index) {
         if (index < 0 || index >= size)
             return;
@@ -83,13 +78,3 @@ class MyLinkedList {
         size--;
     }
 }
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
