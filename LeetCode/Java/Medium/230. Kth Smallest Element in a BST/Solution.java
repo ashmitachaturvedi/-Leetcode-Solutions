a1e9e549f1f84205865d6f2a7444acc333ce2bab
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-08-31 10:33:39 +0530

    Add Medium Java Solution for 230. Kth Smallest Element in a BST from LeetCode

--- LeetCode/Java/Medium/230. Kth Smallest Element in a BST/Solution.java
+++ LeetCode/Java/Medium/230. Kth Smallest Element in a BST/Solution.java
@@ -13,38 +13,38 @@
  *     }
  * }
  */
-// class Solution {
-//     public int kthSmallest(TreeNode root, int k) {
-//         ArrayList ans = new ArrayList<>();
-//         Inorder(root,ans);
-//         return ans.get(k-1);
-//     }
-//     void Inorder(TreeNode root,ArrayList<Integer> ans){
-//         if(root == null) return;
-//         Inorder(root.left,ans);
-//         ans.add(root.val);
-//         Inorder(root.right,ans);
-//     }
-// }
 class Solution {
-
     public int kthSmallest(TreeNode root, int k) {
-
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        Inorder(root, arr);
-
-        return arr.get(k - 1);
+        ArrayList<Integer> ans = new ArrayList<>();
+        Inorder(root,ans);
+        return ans.get(k-1);
     }
-
-    void Inorder(TreeNode root, ArrayList<Integer> arr) {
-
-        if (root == null) return;
-
-        Inorder(root.left, arr);
-
-        arr.add(root.val);
-
-        Inorder(root.right, arr);
+    void Inorder(TreeNode root,ArrayList<Integer> ans){
+        if(root == null) return;
+        Inorder(root.left,ans);
+        ans.add(root.val);
+        Inorder(root.right,ans);
     }
 }
+// class Solution {
+
+//     public int kthSmallest(TreeNode root, int k) {
+
+//         ArrayList<Integer> arr = new ArrayList<>();
+
+//         Inorder(root, arr);
+
+//         return arr.get(k - 1);
+//     }
+
+//     void Inorder(TreeNode root, ArrayList<Integer> arr) {
+
+//         if (root == null) return;
+
+//         Inorder(root.left, arr);
+
+//         arr.add(root.val);
+
+//         Inorder(root.right, arr);
+//     }
+// }
