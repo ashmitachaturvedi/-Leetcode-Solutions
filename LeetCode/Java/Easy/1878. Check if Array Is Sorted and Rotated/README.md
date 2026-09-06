6756dd9747f84debbda627600ea23141fac3a79f
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-06 13:27:51 +0530

    Add Easy Java Solution for 1878. Check if Array Is Sorted and Rotated from LeetCode

--- LeetCode/Java/Easy/1878. Check if Array Is Sorted and Rotated/README.md
+++ LeetCode/Java/Easy/1878. Check if Array Is Sorted and Rotated/README.md
@@ -8,8 +8,8 @@
 Array
 
 ### 🚀 Performance
-- **Runtime:** 2 ms
-- **Memory:** 44 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/1878. Check if Array Is Sorted and Rotated/Solution.java
+++ LeetCode/Java/Easy/1878. Check if Array Is Sorted and Rotated/Solution.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int n = nums.length;
+//         for(int x = 0 ; x < n ; x++){
+//             int[] nums2 = new int[n];
+//             for(int i = 0 ; i < n ; i++){
+//                 nums2[i] = nums[(i+x)%n];
+//             }
+//             boolean sorted = true;
+//             for (int i = 1; i < n; i++) {
+//                 if (nums2[i] < nums2[i - 1]) {
+//                     sorted = false;
+//                     break;
+//                 }
+//             }
+//             if (sorted) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-        for(int x = 0 ; x < n ; x++){
-            int[] nums2 = new int[n];
-            for(int i = 0 ; i < n ; i++){
-                nums2[i] = nums[(i+x)%n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
-            boolean sorted = true;
-            for (int i = 1; i < n; i++) {
-                if (nums2[i] < nums2[i - 1]) {
-                    sorted = false;
-                    break;
-                }
-            }
-            if (sorted) {
-                return true;
+
+            if (count > 1) {
+                return false;
             }
         }
-        return false;
+
+        return true;
     }
 }
