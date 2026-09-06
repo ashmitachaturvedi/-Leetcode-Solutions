618221d1e9e1ee18b81a27d2d10e3658848e7168
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-06 13:25:25 +0530

    Add Easy Java Solution for 1752. Check if Array Is Sorted and Rotated from LeetCode

---
+++ LeetCode/Patterns/Arrays/1752. Check if Array Is Sorted and Rotated/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        for(int x = 0 ; x < n ; x++){
+            int[] nums2 = new int[n];
+            for(int i = 0 ; i < n ; i++){
+                nums2[i] = nums[(i+x)%n];
+            }
+            boolean sorted = true;
+            for (int i = 1; i < n; i++) {
+                if (nums2[i] < nums2[i - 1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+            if (sorted) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
