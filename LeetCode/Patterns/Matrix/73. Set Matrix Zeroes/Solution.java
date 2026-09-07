49d4abd122c8f25910b73ce4dce1f7bb507edff4
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-07 22:57:21 +0530

    Add Medium Java Solution for 73. Set Matrix Zeroes from LeetCode

--- LeetCode/Patterns/Matrix/73. Set Matrix Zeroes/Solution.java
+++ LeetCode/Patterns/Matrix/73. Set Matrix Zeroes/Solution.java
@@ -1,39 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int col0 = 1;
-
-        // Mark rows and columns
-        for(int i = 0; i < m; i++) {
-
-            if(matrix[i][0] == 0)
-                col0 = 0;
-
-            for(int j = 1; j < n; j++) {
-
-                if(matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean[n];
+        // find original zeroes
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    column[j] = true;
                 }
             }
         }
-
-        // Fill zeros using markers
-        for(int i = m - 1; i >= 0; i--) {
-
-            for(int j = n - 1; j >= 1; j--) {
-
-                if(matrix[i][0] == 0 ||
-                   matrix[0][j] == 0) {
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(row[i] || column[j]){
                     matrix[i][j] = 0;
                 }
             }
-
-            if(col0 == 0)
-                matrix[i][0] = 0;
         }
     }
 }
