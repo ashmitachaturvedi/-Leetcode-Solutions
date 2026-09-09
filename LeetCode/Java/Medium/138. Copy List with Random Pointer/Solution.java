0a97c28151cbe8aea0688100fe319945923b2647
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-09 19:17:19 +0530

    Add Medium Java Solution for 138. Copy List with Random Pointer from LeetCode

--- LeetCode/Java/Medium/138. Copy List with Random Pointer/Solution.java
+++ LeetCode/Java/Medium/138. Copy List with Random Pointer/Solution.java
@@ -19,7 +19,6 @@ class Solution {
 
         Node curr = head;
         while(curr != null){
-            
             Node copy = new Node(curr.val);
             copy.next = curr.next;
             curr.next = copy;
