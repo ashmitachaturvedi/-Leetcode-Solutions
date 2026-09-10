6b9aede9da5c2a31796bf1ceac598bdc5e254fa3
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-10 10:44:01 +0530

    Add Easy Java Solution for 501. Find Mode in Binary Search Tree from LeetCode

--- LeetCode/Java/Easy/501. Find Mode in Binary Search Tree/README.md
+++ LeetCode/Java/Easy/501. Find Mode in Binary Search Tree/README.md
@@ -8,8 +8,8 @@
 Tree, Depth-First Search, Binary Search Tree, Binary Tree
 
 ### 🚀 Performance
-- **Runtime:** 8 ms
-- **Memory:** 46.4 MB
+- **Runtime:** 0 ms
+- **Memory:** 47.2 MB
 
 ---
 
--- LeetCode/Java/Easy/501. Find Mode in Binary Search Tree/Solution.java
+++ LeetCode/Java/Easy/501. Find Mode in Binary Search Tree/Solution.java
@@ -13,31 +13,80 @@
  *     }
  * }
  */
-class Solution {
-    public int[] findMode(TreeNode root) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        inorder(root,map);
-        int max = 0;
-        for(int count : map.values()){
-            max = Math.max(max,count);
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int key : map.keySet()){
-            if(map.get(key) == max){
-                ans.add(key);
-            }
-        }
-            int[] result = new int[ans.size()];
-            for(int i = 0 ; i < ans.size() ; i++){
-                result[i] = ans.get(i);
-            }
-            return result;
-    }
-    public void inorder(TreeNode root,HashMap<Integer,Integer> map){
-        if(root == null) return;
-        map.put(root.val,map.getOrDefault(root.val,0)+1);
+// class Solution {
+//     public int[] findMode(TreeNode root) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         inorder(root,map);
+//         int max = 0;
+//         for(int count : map.values()){
+//             max = Math.max(max,count);
+//         }
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         for(int key : map.keySet()){
+//             if(map.get(key) == max){
+//                 ans.add(key);
+//             }
+//         }
+//             int[] result = new int[ans.size()];
+//             for(int i = 0 ; i < ans.size() ; i++){
+//                 result[i] = ans.get(i);
+//             }
+//             return result;
+//     }
+//     public void inorder(TreeNode root,HashMap<Integer,Integer> map){
+//         if(root == null) return;
+//         map.put(root.val,map.getOrDefault(root.val,0)+1);
 
-        inorder(root.left,map);
-        inorder(root.right,map);
+//         inorder(root.left,map);
+//         inorder(root.right,map);
+//     }
+// }
+
+class Solution {
+
+    int maxCount = 0;
+    int curCount = 0;
+    int prev = Integer.MIN_VALUE;
+
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    public void inorder(TreeNode root) {
+
+        if (root == null) return;
+
+        inorder(root.left);
+
+        int cur = root.val;
+
+        if (cur == prev)
+            curCount++;
+        else
+            curCount = 1;
+
+        if (curCount > maxCount) {
+            maxCount = curCount;
+            ans.clear();
+            ans.add(cur);
+        }
+        else if (curCount == maxCount) {
+            ans.add(cur);
+        }
+
+        prev = cur;
+
+        inorder(root.right);
+    }
+
+    public int[] findMode(TreeNode root) {
+
+        inorder(root);
+
+        int[] result = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+
+        return result;
     }
 }
