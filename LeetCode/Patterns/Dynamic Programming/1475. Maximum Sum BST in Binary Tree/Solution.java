1be0947333f1f2865430864e336d76912263ce90
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-20 16:09:12 +0530

    Add Hard Java Solution for 1475. Maximum Sum BST in Binary Tree from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/1475. Maximum Sum BST in Binary Tree/Solution.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// public class Info{
+//     int sum ;
+//     int maxVal;
+//     int minVal;
+//     Info(int sum , int maxVal , int minVal){
+//         this.sum = sum;
+//         this.maxVal = maxVal;
+//         this.minVal = minVal;
+//     }
+// }
+// class Solution {
+//     int maxSum = 0;8
+//     public int maxSumBST(TreeNode root) {
+        
+//     }
+// }
+class Solution {
+
+    int ans = 0;
+
+    class Info {
+        boolean isBST;
+        int min;
+        int max;
+        int sum;
+
+        Info(boolean isBST, int min, int max, int sum) {
+            this.isBST = isBST;
+            this.min = min;
+            this.max = max;
+            this.sum = sum;
+        }
+    }
+
+    public int maxSumBST(TreeNode root) {
+        solve(root);
+        return ans;
+    }
+
+    private Info solve(TreeNode root) {
+
+        if (root == null) {
+            return new Info(true, Integer.MAX_VALUE,
+                            Integer.MIN_VALUE, 0);
+        }
+
+        Info left = solve(root.left);
+        Info right = solve(root.right);
+
+        if (left.isBST && right.isBST &&
+            left.max < root.val &&
+            root.val < right.min) {
+
+            int sum = left.sum + root.val + right.sum;
+
+            ans = Math.max(ans, sum);
+
+            int min = Math.min(root.val, left.min);
+            int max = Math.max(root.val, right.max);
+
+            return new Info(true, min, max, sum);
+        }
+
+        return new Info(false, 0, 0, 0);
+    }
+}
