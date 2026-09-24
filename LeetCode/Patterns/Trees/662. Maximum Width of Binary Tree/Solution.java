37c0094686e2578c08744a8fde0effb2b94a020b
Ashmita Chaturvedi <ashmitachaturvedi3@gmail.com>
2026-09-24 22:00:21 +0530

    Add Medium Java Solution for 662. Maximum Width of Binary Tree from LeetCode

---
+++ LeetCode/Patterns/Trees/662. Maximum Width of Binary Tree/Solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair{
+        TreeNode node;
+        long index;
+        Pair(TreeNode node, long index){
+            this.node = node;
+            this.index = index;
+        }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root,0));
+        int maxWidth = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            long first = q.peek().index;
+            long last = first;
+            for(int i = 0 ; i < size ; i++){
+                Pair curr = q.poll();
+                long index = curr.index;
+                last = index;
+                if(curr.node.left != null) q.offer(new Pair(curr.node.left, 2 * index));
+                if(curr.node.right != null) q.offer(new Pair(curr.node.right , 2 * index + 1));
+            }
+            maxWidth = Math.max(maxWidth, (int)(last - first + 1));
+        }
+        return maxWidth;
+    }
+}
